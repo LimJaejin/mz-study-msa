6233a5a271a11d3e3c8ea51e20bec2c1b32f3be8
임재진(메가존) <mzjjlim@lgupluspartners.co.kr>
2021-11-04 18:04:41 +0900

    Layer별 기본 Config 클래스 수정

--- subprojects/infrastructure-di/src/main/java/com/lguplus/fleta/config/InfrastructureDiConfig.java
+++ subprojects/infrastructure-di/src/main/java/com/lguplus/fleta/config/InfrastructureDiConfig.java
@@ -6,19 +6,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EntityScan(basePackages = {"com.lguplus.fleta"})
-public class InfrastructureConfig {
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-            .type(HikariDataSource.class)
-            .build();
-    }
+public class InfrastructureDiConfig {
 }
