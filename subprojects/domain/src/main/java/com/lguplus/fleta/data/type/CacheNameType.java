f7df334ed3c1c95f42ec0878f3a8b9d1d04213d4
임재진(메가존) <mzjjlim@lgupluspartners.co.kr>
2021-12-27 16:14:21 +0900

    캐시네임 클래스 리팩토링

--- subprojects/domain/src/main/java/com/lguplus/fleta/data/type/CacheNameType.java
+++ subprojects/domain/src/main/java/com/lguplus/fleta/data/type/CacheNameType.java
@@ -6,15 +6,15 @@ import java.time.Duration;
 
 public enum CacheNameType {
 
-    TTL_1_MINUTE("TTL1M", Duration.ofMinutes(1)),
-    TTL_5_MINUTE("TTL5M", Duration.ofMinutes(5)),
-    TTL_10_MINUTE("TTL10M", Duration.ofMinutes(10)),
-    TTL_15_MINUTE("TTL15M", Duration.ofMinutes(15)),
-    TTL_30_MINUTE("TTL30M", Duration.ofMinutes(30)),
-    TTL_1_HOUR("TTL1H", Duration.ofHours(1)),
-    TTL_2_HOUR("TTL2H", Duration.ofHours(2)),
-    TTL_1_DAY("TTL1D", Duration.ofDays(1)),
-    TTL_2_DAY("TTL2D", Duration.ofDays(2));
+    TTL_1_MINUTE(Constant.TTL_1_MINUTE, Duration.ofMinutes(1)),
+    TTL_5_MINUTE(Constant.TTL_5_MINUTE, Duration.ofMinutes(5)),
+    TTL_10_MINUTE(Constant.TTL_10_MINUTE, Duration.ofMinutes(10)),
+    TTL_15_MINUTE(Constant.TTL_15_MINUTE, Duration.ofMinutes(15)),
+    TTL_30_MINUTE(Constant.TTL_30_MINUTE, Duration.ofMinutes(30)),
+    TTL_1_HOUR(Constant.TTL_1_HOUR, Duration.ofHours(1)),
+    TTL_2_HOUR(Constant.TTL_2_HOUR, Duration.ofHours(2)),
+    TTL_1_DAY(Constant.TTL_1_DAY, Duration.ofDays(1)),
+    TTL_2_DAY(Constant.TTL_2_DAY, Duration.ofDays(2));
 
     @Getter
     private final String cacheName;
@@ -24,5 +24,25 @@ public enum CacheNameType {
     CacheNameType(String cacheName, Duration duration) {
         this.cacheName = cacheName;
         this.duration = duration;
+    }
+
+    @Override public String toString() {
+        return super.toString();
+    }
+
+
+    public static class Constant {
+
+        public static final String TTL_1_MINUTE = "TTL1M";
+        public static final String TTL_5_MINUTE = "TTL5M";
+        public static final String TTL_10_MINUTE = "TTL10M";
+        public static final String TTL_15_MINUTE = "TTL15M";
+        public static final String TTL_30_MINUTE = "TTL30M";
+        public static final String TTL_1_HOUR = "TTL1H";
+        public static final String TTL_2_HOUR = "TTL2H";
+        public static final String TTL_1_DAY = "TTL1D";
+        public static final String TTL_2_DAY = "TTL2D";
+
+        private Constant() { }
     }
 }
