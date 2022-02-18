904b687fd05c14710d68c9d8d0b33d804092443f
전강욱(메가존) <mzkwjun@lgupluspartners.co.kr>
2022-02-18 17:23:14 +0900

    코드 포맷터 import 레이아웃 수정

--- subprojects/presentation/src/main/java/com/lguplus/fleta/data/annotation/AlphabetOrEmptyPattern.java
+++ subprojects/presentation/src/main/java/com/lguplus/fleta/data/annotation/AlphabetOrEmptyPattern.java
@@ -1,12 +1,12 @@
 package com.lguplus.fleta.data.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.regex.Pattern;
 import javax.validation.Constraint;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import javax.validation.Payload;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import java.util.regex.Pattern;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -14,17 +14,21 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Target(FIELD)
 @Retention(RUNTIME)
 @Constraint(validatedBy = AlphabetOrEmptyPattern.AlphabetPatternValidator.class)
-public @interface AlphabetOrEmptyPattern
-{
+public @interface AlphabetOrEmptyPattern {
+
     String message() default "영문자만 사용할수 있습니다.";
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 
     class AlphabetPatternValidator implements ConstraintValidator<AlphabetOrEmptyPattern, String> {
+
         @Override
         public boolean isValid(String value, ConstraintValidatorContext context) {
-            if(value == null || value.length() == 0)
+            if (value == null || value.length() == 0) {
                 return true;
+            }
 
             Pattern pattern = Pattern.compile("^[a-zA-Z]*$");
             return pattern.matcher(value).find();
